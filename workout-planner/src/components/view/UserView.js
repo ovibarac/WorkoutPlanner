59b9059ef5ee8481bb95ff982c0e98eed1096c5b
Ovidiu Barac <ovy.barak@gmail.com>
2023-05-29 16:30:50 +0300

    files

--- workout-planner/src/components/view/UserView.js
+++ workout-planner/src/components/view/UserView.js
@@ -4,11 +4,6 @@ import {Button, Drawer, SwipeableDrawer} from '@mui/material';
 import {useState} from "react";
 
 const UserView = (props) => {
-    //TODO sa nu dea eroare cand e goala lista
-    //TODO meniu
-    //TODO daca mai am timp add workout to day daca nu is 6 funct oricum
-    //TODO change workout model to include many to many relationship with exercises
-    //TODO workout UI to add dar cred ca il las exact ca exercise cel putin la inceput
     const [openDrawer, setOpenDrawer] = useState(false);
     return(
         <>
